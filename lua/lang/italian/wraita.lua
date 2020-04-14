2936c3613b70189d0f4c92771eedaafdf81d0f72
Alessandro Levante <62677560+PinoMartirio@users.noreply.github.com>
2020-04-14 22:22:56 +0200

    Added Italian tranlsation
    Added full Italian translation (except the name, after suggestions) for the role Wrath.

---
+++ lua/lang/italian/wraita.lua
@@ -0,0 +1,12 @@
+L = LANG.GetLanguageTableReference("italiano")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[WRATH.name] = "Wrath"
+L["info_popup_" .. WRATH.name] = [[Sei il Wrath, collabora con gli innocenti per vincere!]]
+L["body_found_" .. WRATH.abbr] = "Era un Wrath."
+L["search_role_" .. WRATH.abbr] = "Questa persona era un Wrath!"
+L["target_" .. WRATH.name] = "Wrath"
+L["ttt2_desc_" .. WRATH.name] = [[Il Wrath è un innocente che collabora con gli altri innocenti per vincere. Però, se viene ucciso da un altro innocente, tornerà in vita
+ e diventerà un traditore.]]
+
+-- OTHER ROLE LANGUAGE STRINGS
