8db6498f38ffbb29812adef028fa44bf65857d82
Tekiad <tekiadp@gmail.cm>
2020-07-31 03:29:16 -0300

    Create wraesp.lua

---
+++ lua/lang/spanish/wraesp.lua
@@ -0,0 +1,14 @@
+L = LANG.GetLanguageTableReference("spanish")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[WRATH.name] = "Rabioso"
+L["info_popup_" .. WRATH.name] = [[Eres un Rabioso, trabaja con los inocentes para ganar.]]
+L["body_found_" .. WRATH.abbr] = "¡Era un Rabioso!"
+L["search_role_" .. WRATH.abbr] = "Esta persona era un rabioso."
+L["target_" .. WRATH.name] = "Rabioso"
+L["ttt2_desc_" .. WRATH.name] = [[El Rabioso es un inocente que tiene la chance de revivir. Sin embargo, para que esto sea cumplido, debe ser asesinado 
+por un inocente. Si esto sucede, revivirá como traidor.]]
+
+-- OTHER ROLE LANGUAGE STRINGS
+
+L["ttt2_role_wrath_revival_message"] = "You will be revived as a Traitor!"
