969247538791f67e917682abccdabb241623586a
Blueberryy <igory.ygr200@gmail.com>
2020-09-27 12:33:32 +0500

    Create wraeng.lua

---
+++ lua/lang/russian/wraeng.lua
@@ -0,0 +1,14 @@
+L = LANG.GetLanguageTableReference("Русский")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[WRATH.name] = "Гнев"
+L["info_popup_" .. WRATH.name] = [[Вы Гнев, работай с невинными, чтобы победить!]]
+L["body_found_" .. WRATH.abbr] = "Он был Гневом."
+L["search_role_" .. WRATH.abbr] = "Этот человек был Гневом!"
+L["target_" .. WRATH.name] = "Гнев"
+L["ttt2_desc_" .. WRATH.name] = [[Гнев - это невинная роль, и она работает с невиновными для победы. Однако, если его убьет другой невиновный, он будет воскрешен,
+ и превратился в предателя.]]
+
+-- OTHER ROLE LANGUAGE STRINGS
+
+L["ttt2_role_wrath_revival_message"] = "Вы будете воскрешены как Предатель!"
