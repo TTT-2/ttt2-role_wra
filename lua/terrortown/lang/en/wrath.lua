593afd9abb451edc0ffe66611564859f30371dc9
SrB54 <srb5467@gmail.com>
2021-11-04 18:10:13 +0000

    Sentence fix and gender

--- lua/terrortown/lang/en/wrath.lua
+++ lua/terrortown/lang/en/wrath.lua
@@ -6,7 +6,7 @@ L["info_popup_" .. WRATH.name] = [[You are a Wrath, work with the innocents to w
 L["body_found_" .. WRATH.abbr] = "They were a Wrath."
 L["search_role_" .. WRATH.abbr] = "This person was a Wrath!"
 L["target_" .. WRATH.name] = "Wrath"
-L["ttt2_desc_" .. WRATH.name] = [[The Wrath is an innocent role and works with the innocents to win. However if he is killed by another innocent he will be revived
+L["ttt2_desc_" .. WRATH.name] = [[The Wrath is an innocent role and works with the innocents to win. However, if they are killed by another innocent, they will be revived
  and turned into a traitor.]]
 
 -- OTHER ROLE LANGUAGE STRINGS
