2913d85232ebfa45f91575c427f99d5235d9840c
sbzl <92261479+sbzlzh@users.noreply.github.com>
2022-09-03 00:57:18 +0800

    Add simplified Chinese Translation

---
+++ lua/terrortown/lang/zh_hans/wrath.lua
@@ -0,0 +1,14 @@
+L = LANG.GetLanguageTableReference("zh_hans")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[WRATH.name] = "二五仔"
+L["info_popup_" .. WRATH.name] = [[你是一个二五仔,和无辜的人一起工作,赢得胜利!]]
+L["body_found_" .. WRATH.abbr] = "他们是二五仔."
+L["search_role_" .. WRATH.abbr] = "这个人是二五仔!"
+L["target_" .. WRATH.name] = "二五仔"
+L["ttt2_desc_" .. WRATH.name] = [[二五仔是一个无辜的角色,与无辜者合作以赢得胜利.然而,如果他们被另一个无辜者杀害,他们将复活
+变成了叛徒.]]
+
+-- OTHER ROLE LANGUAGE STRINGS
+
+L["ttt2_role_wrath_revival_message"] = "你会复活为叛徒!"
